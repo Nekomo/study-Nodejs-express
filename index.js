23ee6fe29d81a3893fd08fb55aa8eace5899ac25
Nejomo <daisanbukan@gmail.com>
2019-04-01 01:58:58 +0900

    ejbのレンダリング処理w  を追加

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+/**
+ * モジュール・ロード
+ */
 let express = require('express');
+let ejs = require('ejs');
+
 let app = express();
 
-app.get('/',(req,res)=>{
-        res.send('Welcome to Express!')
-        })
-app.listen(3000,()=>{
+//レンダリングエンジンの設定。今回はejsを用いる
+app.engine('ejs', ejs.renderFile);
+
+app.get("/", (req, res) => {
+        //index.ejsのレンダリング
+        res.render('index.ejs', {
+                title: 'Index',
+                content: 'This is Express app Top page!'
+        });
+});
+let server = app.listen(3000, () => {
         console.log('Start server port:3000')
-        })
+})
